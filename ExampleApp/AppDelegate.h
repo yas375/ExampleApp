14bd2f98b2bc311d9b723c8cd42442d896b957e1
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2013-10-28 09:28:52 +0300

    Empty xcode project

---
+++ ExampleApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExampleApp
+//
+//  Created by Victor Ilyukevich on 10/28/13.
+//  Copyright (c) 2013 Victor Ilyukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ExampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExampleApp
+//
+//  Created by Victor Ilyukevich on 10/28/13.
+//  Copyright (c) 2013 Victor Ilyukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
