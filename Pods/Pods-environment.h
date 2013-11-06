2f4d6f32b758956dd32b5ce448525f5b70c6a184
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2013-11-06 13:22:51 +0300

    Added CSNotificationView as a pod

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CSNotificationView
+#define COCOAPODS_POD_AVAILABLE_CSNotificationView
+#define COCOAPODS_VERSION_MAJOR_CSNotificationView 0
+#define COCOAPODS_VERSION_MINOR_CSNotificationView 3
+#define COCOAPODS_VERSION_PATCH_CSNotificationView 0
+
