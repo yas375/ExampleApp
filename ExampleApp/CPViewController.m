de41d1c06c39ab245edc04f550bb0c8931fa0e56
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2013-11-06 13:32:50 +0300

    Example usage of CSNotificationview

--- ExampleApp/CPViewController.m
+++ ExampleApp/CPViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Victor Ilyukevich. All rights reserved.
 //
 
+#import <CSNotificationView/CSNotificationView.h>
 #import "CPViewController.h"
 
 @interface CPViewController ()
@@ -14,16 +15,13 @@
 
 @implementation CPViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super viewDidAppear:animated];
+  [CSNotificationView showInViewController:self
+                                     style:CSNotificationViewStyleError
+                                   message:@"A critical error happened."];
 }
 
 @end
