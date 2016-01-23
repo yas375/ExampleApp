41b50e4b7fbff5f772003017863dfc735c7e1b23
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2016-01-22 18:12:17 -0800

    Verifying borders when playes is inside scrollview
    > convert -size 960x708 xc:#eeeeee canvas_1.png
    > for i in {2..93}; do cp canvas_1.png canvas_$i.png; done
    > ffmpeg -framerate 30 -i canvas_%d.png -c:v libx264 -r 30 -pix_fmt yuv420p video.mp4

--- PlayerInsideScrollView/PlayerView.swift
+++ PlayerInsideScrollView/PlayerView.swift
@@ -7,6 +7,7 @@
 //
 
 import AVFoundation
+import UIKit
 
 class PlayerView: UIView {
 
--- PlayerInsideScrollView/ViewController.swift
+++ PlayerInsideScrollView/ViewController.swift
@@ -6,9 +6,24 @@
 //  Copyright © 2016 Victor. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var playerView: PlayerView!
 
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let url = NSBundle.mainBundle().URLForResource("video", withExtension: "mp4") {
+      playerView.player = AVPlayer(URL: url)
+    }
+  }
+
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    playerView.player?.play()
+  }
+  
 }
 
