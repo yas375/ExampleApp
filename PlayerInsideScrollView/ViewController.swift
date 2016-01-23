6e083ecd4898a370c1be546fe87611d1cf02b738
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2016-01-22 18:29:21 -0800

    Extend example with AVPlayerViewController
    Still getting flickering edges on scrolling.

--- PlayerInsideScrollView/ViewController.swift
+++ PlayerInsideScrollView/ViewController.swift
@@ -7,23 +7,37 @@
 //
 
 import AVFoundation
+import AVKit
 import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet var playerView: PlayerView!
 
+  func makePlayer() -> AVPlayer? {
+    if let url = NSBundle.mainBundle().URLForResource("video", withExtension: "mp4") {
+      return AVPlayer(URL: url)
+    }
+    return nil
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    if let url = NSBundle.mainBundle().URLForResource("video", withExtension: "mp4") {
-      playerView.player = AVPlayer(URL: url)
-    }
+
+    playerView.player = self.makePlayer()
   }
 
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated)
     playerView.player?.play()
   }
-  
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "embedPlayer" {
+      let controller = segue.destinationViewController as! AVPlayerViewController
+      controller.player = self.makePlayer()
+      controller.showsPlaybackControls = false
+      controller.player?.play() // edges are flickering even if we don't play the video
+    }
+  }
 }
 
